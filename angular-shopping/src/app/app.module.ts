42037a24335c7ca24b43d403569f04404781f8ed
kikeAKAdelta <kike04arevalo@gmail.com>
2020-07-19 21:56:22 -0600

    Add Routing

--- angular-shopping/src/app/app.module.ts
+++ angular-shopping/src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MenuBarComponent} from './menu-bar/menu-bar.component';
 import { HomeComponent } from './home/home.component';
 import { FootPageComponent } from './foot-page/foot-page.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     HomeComponent,
-    FootPageComponent
+    FootPageComponent,
+    ProductsListComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'products', component: ProductsListComponent},
+      {path: 'about', component: AboutUsComponent},
+      {path: 'contact', component: ContactUsComponent}
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-shopping/src/app/products-list/products-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'products-list',
+    templateUrl: './products-list.component.html',
+    styleUrls: ['./products-list.component.css']
+})
+
+export class ProductsListComponent{
+
+    constructor(){}
+
+}
