2234eb22a416081ee223496f6626b5db5050537a
kikeAKAdelta <kike04arevalo@gmail.com>
2020-07-19 19:12:32 -0600

    HomeComponent

--- angular-shopping/src/app/app.module.ts
+++ angular-shopping/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuBarComponent} from './menu-bar/menu-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
